fe0aba8c603e18384e3bfd3b9fadc6cad9f5dc72
dev-lima31 <ubertipedro@gmail.com>
2025-04-04 16:03:23 -0300

    first

--- obj/Debug/net8.0/Pedro Henrique - 21.03.2025.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pedro Henrique - 21.03.2025.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pedro Henrique - 21.03.2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a36b7f7b3589ef99de3c8625912303a5fb762697")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pedro Henrique - 21.03.2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pedro Henrique - 21.03.2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
